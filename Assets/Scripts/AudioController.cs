5e914f52857c84679e77f1fb9b5970ffba9270f1
Borys <borys.duchewicz@op.pl>
2019-12-04 22:14:00 +0100

    Projekt v1.1
    Poprawiony bug z wpadaniem w klocki

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -6,18 +6,25 @@ public class AudioController : MonoBehaviour
 {
     // Start is called before the first frame update
     public AudioClip MusicClip;
-    public  AudioSource MusicSource;
-    void Start()
+    public  AudioSource musicSource;
+    private float musicVolume = 1f;
+    void Start() 
     {
-        MusicSource.clip = MusicClip;
+        musicSource.clip = MusicClip;
     }
 
     // Update is called once per frame
     void Update()
     {
+        musicSource.volume = musicVolume;
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            MusicSource.Play();
+            musicSource.Play();
         }
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
     }
 }
