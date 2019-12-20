1d1c029db2d5ab0af4e270ef07dad493f0628d3a
T_4 <slawek.junior@interia.eu>
2019-12-20 10:14:41 +0100

    Ułomny ruch

---
+++ Assets/Scripts/VillanController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Vector2 = UnityEngine.Vector2;
+using System.Diagnostics;
+using System;
+
+public class VillanController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 0.01f;
+    public float jumpForce = 3f;
+
+    private Rigidbody2D rigidBody;
+    private BoxCollider2D boxCollider;
+    public Stopwatch timer;
+
+
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        
+        rigidBody.velocity = new Vector2(0.5f, 0.5f);
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(MoveVillanTask());
+
+    }
+    
+    IEnumerator MoveVillanTask()
+    {
+        yield return new WaitForSeconds(1);
+        rigidBody.velocity = new Vector2(1f, 3f);
+        yield return new WaitForSeconds(1);
+        rigidBody.velocity = new Vector2(-1f, -3f);
+        
+    }
+
+}
