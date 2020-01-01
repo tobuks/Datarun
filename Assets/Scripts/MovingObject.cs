6083a9c5ba1b40c7e4aff0ca9ee2a66c5fbd0ea7
Borys <Borys.Duchewicz@op.pl>
2020-01-01 17:06:15 +0100

    4 poziom
    Ruszające się obiekty 
    3 sceny narysowane

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -4,55 +4,41 @@ using UnityEngine;
 public class MovingObject : MonoBehaviour
 {
 
-/*    public float speed;
+    public float speed;
     private bool movingRight = true;
+    private bool stop = false;
     public Transform groundDetection;
     void Update()
     {
-        transform.Translate(Vector2.right*speed*Time.deltaTime);
+        if (!stop)
+        {
+            transform.Translate(Vector2.right*speed*Time.deltaTime);
+        }
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position,Vector2.down,2f);
-        if (groundInfo.collider.gameObject.tag != "plat")
+        if (!groundInfo.collider)
         {
             if (movingRight)
             {
-                transform.eulerAngles= new Vector3(0,-180,0);
+                stop = true;
+                StartCoroutine(Move());
+                transform.eulerAngles = new Vector3(0, -180, 0);
                 movingRight = false;
             }
             else
             {
-                transform.eulerAngles= new Vector3(0,0,0);
+                stop = true;
+                StartCoroutine(Move());
+                transform.eulerAngles = new Vector3(0, 0, 0);
                 movingRight = true;
             }
         }
-    }*/
-    private Rigidbody2D rb;
-
-    public float moveTime;
-    public float stopTime;
-
-    public float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        StartCoroutine(Move());
     }
+
     private IEnumerator Move()
     {
-        while (true)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            rb.velocity = new Vector2(speed, 0);
-            yield return new WaitForSeconds(moveTime);
-            rb.velocity= new Vector2(0,0);
-            yield return  new WaitForSeconds(stopTime);
-            transform.eulerAngles = new Vector3(0, -180, 0);
-            rb.velocity = new Vector2(-speed, 0);
-            yield return new WaitForSeconds(moveTime);
-            rb.velocity = new Vector2(0, 0);
-            yield return new WaitForSeconds(stopTime);
-        }
+        yield return  new WaitForSeconds(2);
+        stop = false;
+     
     }
 
-  
 }
