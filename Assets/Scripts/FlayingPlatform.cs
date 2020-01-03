ab27ec8cdeae1ba3d1fd763145ae3963a1046a08
Borys <Borys.Duchewicz@op.pl>
2020-01-03 14:06:05 +0100

    Poprawki 2 i 3 poziom
    Ułatwienie 3 poziomu 
    Poprawka textur 2 poziomu postać nie wpada już w ziemie przed wentylatorami

--- Assets/Scripts/FlayingPlatform.cs
+++ Assets/Scripts/FlayingPlatform.cs
@@ -7,25 +7,29 @@ public class FlayingPlatform : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField] public GameObject myPrefabs;
     private Vector3 position;
-    
+
+  
     // Start is called before the first frame update
     void Start()
     {
+        
         rb = GetComponent<Rigidbody2D>();
         position = transform.position;
-        StartCoroutine(Spawn(position));
+        StartCoroutine(Spawn());
     }
-    private IEnumerator Spawn(Vector2 position)
+  
+    private IEnumerator Spawn()
     {
-        rb.velocity = new Vector2(0, 6f);
-        yield return new WaitForSeconds(Random.Range(1, 5f));
+        
+        rb.velocity = new Vector2(0, 5f);
+        yield return new WaitForSeconds(Random.Range(1.5f, 5f));
         Instantiate(myPrefabs, position, Quaternion.identity);
 
     }
     // Update is called once per frame
     void Update()
     {
-        if (rb.transform.position.y > 73)
+        if (rb.transform.position.y > 68)
         {
             Destroy(gameObject);
         }
